bcaa302d285a68f771d502907fc1f90a8c0d488d
harish-fueled <harish@fueled.co>
2015-08-20 16:32:23 +0530

    Chore(Frameworks): Added Utilities Framework

---
+++ EthanolContacts/EthanolContacts.h
@@ -0,0 +1,19 @@
+//
+//  EthanolContacts.h
+//  EthanolContacts
+//
+//  Created by hhs-fueled on 20/08/15.
+//  Copyright © 2015 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EthanolContacts.
+FOUNDATION_EXPORT double EthanolContactsVersionNumber;
+
+//! Project version string for EthanolContacts.
+FOUNDATION_EXPORT const unsigned char EthanolContactsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EthanolContacts/PublicHeader.h>
+
+
